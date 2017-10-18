2a50c8e6733b3820a2594118991bb334c6b3ecbc
itgsod-yusuffehmi-sari <32324770+itgsod-yusuffehmi-sari@users.noreply.github.com>
2017-10-18 14:10:46 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 Simple program that calculates fares.
 
+om age är mindre än 20
+    ge halvbiljett
+    
+om age är större 65 
+    ge halvbiljet
+    
+om age är mellan 20 och 65
+    helbiljett
+    
--- sl.se.py
+++ sl.se.py
@@ -1,11 +1,17 @@
 def fares(age, student=False, senior=False):
-    print age,student,senior
-    return 'hel'
+    if student or senior:
+        return 'halv'
+    else:
+        if age < 20 or age>65:
+            return 'halv'
+        else:
+            return 'hel'
 
 
+# test cases
+print 'child 10', fares(10)
+print 'senior 15',fares(15,senior=True)
+print 'student 15',fares(15,student=True)
+print 'adult 30', fares(30)
 
-
-
-fares(10)
-fares(65,senior=True)
-fares(25,student=True)
+print 'hej'
